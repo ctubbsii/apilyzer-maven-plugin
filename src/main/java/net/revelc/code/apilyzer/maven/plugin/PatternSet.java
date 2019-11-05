93c68c751c76641b94328b045a30efd76fc360eb
Christopher Tubbs <ctubbsii@apache.org>
2019-11-04 20:18:59 -0500

    WIP

--- src/main/java/net/revelc/code/apilyzer/maven/plugin/PatternSet.java
+++ src/main/java/net/revelc/code/apilyzer/maven/plugin/PatternSet.java
@@ -14,36 +14,24 @@
 
 package net.revelc.code.apilyzer.maven.plugin;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 class PatternSet {
   private final List<Pattern> patterns;
 
   PatternSet(List<String> regexs) {
-    if (regexs.size() == 0) {
-      patterns = Collections.emptyList();
-    } else {
-      patterns = new ArrayList<>();
-      for (String regex : regexs) {
-        patterns.add(Pattern.compile(regex));
-      }
-    }
+    patterns = regexs.isEmpty() ? Collections.emptyList()
+        : regexs.stream().map(Pattern::compile).collect(Collectors.toList());
   }
 
-  boolean matchesAny(String input) {
-    for (Pattern pattern : patterns) {
-      if (pattern.matcher(input).matches()) {
-        return true;
-      }
-    }
-
-    return false;
+  boolean anyMatch(String input) {
+    return patterns.stream().anyMatch(p -> p.matcher(input).matches());
   }
 
-  public int size() {
-    return patterns.size();
+  public boolean isEmpty() {
+    return patterns.isEmpty();
   }
 }
